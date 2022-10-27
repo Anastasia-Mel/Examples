d09518eaa8590db3fd21452fac6a9e5fb5fad804
Anastasia_Mel <melnik2775@gmail.com>
2022-10-27 18:03:27 +0300

    Example 005 hello

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter User name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, Hello, Masha!");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
